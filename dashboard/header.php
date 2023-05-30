91c4d9926b2dcfc358e8e825b68a59d7e8c575a8
0xbabak <103176930+0xbabak@users.noreply.github.com>
2023-05-30 12:44:51 +0300

    Add files via upload

---
+++ dashboard/header.php
@@ -0,0 +1,147 @@
+<html lang="fa-IR" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="../css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="../css/Font-kalameh.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<!-- strat navbar & container code-->
+<div class="row">
+    <div class="col-3">
+        <div class="multi-collapse collapse show">
+            <nav class="navbar navbar-expand-md navbar-light bg-white fixed-right">
+                <div class="collapse navbar-collapse border-gradient-white-left border-1">
+                    <ul class="navbar flex-column list-unstyled">
+                        <li class="nav-item">
+
+                            <!-- row logo-->
+                            <div class="row justify-align-content-around px-5 mt-3">
+                                <div class="col-10">
+                                    <a href="#" class="text-dark text-start navbar-brand">
+                                        <img src="../images/logo.png" alt="">
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="nav-item">
+                                <!-- row-icon-1 --->
+                                <div class="row align-items-center mt-5 px-5">
+                                    <div class="col-2">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M5 10H7C9 10 10 9 10 7V5C10 3 9 2 7 2H5C3 2 2 3 2 5V7C2 9 3 10 5 10Z" stroke="#838383" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M17 10H19C21 10 22 9 22 7V5C22 3 21 2 19 2H17C15 2 14 3 14 5V7C14 9 15 10 17 10Z" stroke="#838383" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M17 22H19C21 22 22 21 22 19V17C22 15 21 14 19 14H17C15 14 14 15 14 17V19C14 21 15 22 17 22Z" stroke="#838383" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M5 22H7C9 22 10 21 10 19V17C10 15 9 14 7 14H5C3 14 2 15 2 17V19C2 21 3 22 5 22Z" stroke="#838383" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+
+                                    </div>
+                                    <div class="col-10">
+                                        <a href="index.php" class="text-orange dashbod-item-nav">
+                                            داشبورد
+                                        </a>
+                                    </div>
+                                </div>
+                                <!-- row-icon-2 --->
+                                <div class="row align-items-center mt-5 px-5">
+                                    <div class="col-2">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M15.75 9H8.25" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M15.75 15H8.25" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+                                    </div>
+                                    <div class="col-10">
+                                        <a href="accounting.php" class="text-838383 accounting">
+                                            حسابداری
+                                        </a>
+                                    </div>
+                                </div>
+                                <!-- row-icon-3 --->
+                                <div class="row align-items-center mt-5 px-5">
+                                    <div class="col-2">
+                                        <svg class="orders-nav" width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8.40002 8.5H15.6C19 8.5 19.34 10.09 19.57 12.03L20.47 19.53C20.76 21.99 20 24 16.5 24H7.51003C4.00003 24 3.24002 21.99 3.54002 19.53L4.44003 12.03C4.66003 10.09 5.00002 8.5 8.40002 8.5Z" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M8 10V6.5C8 5 9 4 10.5 4H13.5C15 4 16 5 16 6.5V10" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                            <circle cx="18.6333" cy="6.29608" r="5.63333" fill="#FF2323" />
+                                            <path d="M21.4204 3.88526V5.37804C21.4204 5.61375 21.3627 5.83112 21.2475 6.03015C21.1323 6.22919 20.9751 6.38894 20.7761 6.50941C20.5823 6.62465 20.3676 6.68226 20.1319 6.68226C19.9171 6.68226 19.7233 6.63512 19.5505 6.54084C19.3828 6.44132 19.2493 6.30776 19.1498 6.14015C19.045 6.30776 18.9062 6.44132 18.7334 6.54084C18.5657 6.63512 18.3746 6.68226 18.1598 6.68226C17.9189 6.68226 17.7094 6.62203 17.5313 6.50156V9H16.8792V6.38371V6.06944C16.8792 5.60327 16.8661 5.19996 16.8399 4.8595C16.8189 4.51904 16.7692 4.20739 16.6906 3.92455L17.272 3.8067C17.382 4.0843 17.4606 4.59761 17.5077 5.34662C17.5077 5.52994 17.5706 5.68708 17.6963 5.81802C17.822 5.94373 17.9765 6.00658 18.1598 6.00658C18.3431 6.00658 18.4976 5.94373 18.6234 5.81802C18.7543 5.68708 18.8198 5.52994 18.8198 5.34662V4.12097H19.4719V5.34662C19.4719 5.52994 19.5347 5.68708 19.6604 5.81802C19.7914 5.94373 19.9485 6.00658 20.1319 6.00658C20.3099 6.00658 20.4618 5.94373 20.5875 5.81802C20.7132 5.68708 20.7761 5.52994 20.7761 5.34662V3.88526H21.4204Z" fill="white" />
+                                        </svg>
+
+
+
+                                    </div>
+                                    <div class="col-10">
+                                        <a href="orders.php" class="text-838383 orders-nav">
+                                            سفارشات </a>
+                                    </div>
+                                </div>
+                                <!-- row-icon-4 --->
+                                <div class="row align-items-center mt-5 px-5">
+                                    <div class="col-2">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M18 18.86H17.24C16.44 18.86 15.68 19.17 15.12 19.73L13.41 21.42C12.63 22.19 11.36 22.19 10.58 21.42L8.87 19.73C8.31 19.17 7.54 18.86 6.75 18.86H6C4.34 18.86 3 17.53 3 15.89V4.97998C3 3.33998 4.34 2.01001 6 2.01001H18C19.66 2.01001 21 3.33998 21 4.97998V15.89C21 17.52 19.66 18.86 18 18.86Z" stroke="#838383" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M7 9.16003C7 8.23003 7.76 7.46997 8.69 7.46997C9.62 7.46997 10.38 8.23003 10.38 9.16003C10.38 11.04 7.71 11.24 7.12 13.03C7 13.4 7.31 13.77 7.7 13.77H10.38" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M16.0398 13.76V8.05003C16.0398 7.79003 15.8698 7.55997 15.6198 7.48997C15.3698 7.41997 15.0998 7.51997 14.9598 7.73997C14.2398 8.89997 13.4598 10.22 12.7798 11.38C12.6698 11.57 12.6698 11.82 12.7798 12.01C12.8898 12.2 13.0998 12.3199 13.3298 12.3199H16.9998" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+
+                                    </div>
+                                    <div class="col-10">
+                                        <a href="#" class="text-838383">
+                                            پشتیبانی
+                                        </a>
+                                    </div>
+                                </div>
+                                <!-- row-icon-5 --->
+                                <div class="row align-items-center mt-5 px-5">
+                                    <div class="col-2">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M9 2H15C20 2 22 4 22 9V15C22 20 20 22 15 22H9C4 22 2 20 2 15V9C2 4 4 2 9 2Z" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M8.57031 15.27L15.1103 8.72998" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M8.98001 10.37C9.65932 10.37 10.21 9.81936 10.21 9.14005C10.21 8.46073 9.65932 7.91003 8.98001 7.91003C8.3007 7.91003 7.75 8.46073 7.75 9.14005C7.75 9.81936 8.3007 10.37 8.98001 10.37Z" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M15.52 16.09C16.1993 16.09 16.75 15.5393 16.75 14.86C16.75 14.1807 16.1993 13.63 15.52 13.63C14.8407 13.63 14.29 14.1807 14.29 14.86C14.29 15.5393 14.8407 16.09 15.52 16.09Z" stroke="#838383" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+
+                                    </div>
+                                    <div class="col-10">
+                                        <a href="#" class="text-838383">
+                                            کد تخفیف
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+    </div>
+
+    <div class="col-8">
+        <!-- navbar header -->
+        <div class="row navbar navbar-expand navbar-light bg-white pb-5">
+            <div class="col-12">
+                <div class="row justify-content-end align-items-center pe-5">
+                    <div class="col-1"> <a href="#"><svg class="center-img" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M11 20C15.9706 20 20 15.9706 20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20Z" stroke="#1E1E1E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M18.9304 20.6898C19.4604 22.2898 20.6704 22.4498 21.6004 21.0498C22.4504 19.7698 21.8904 18.7198 20.3504 18.7198C19.2104 18.7098 18.5704 19.5998 18.9304 20.6898Z" stroke="#1E1E1E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </a></div>
+                    <div class="col-1 border-start border-end"> <a href="#"><img class="center-img" src="../images/sms.png" class="img-fluid"></a></div>
+                    <div class="col-1"> <a href="#"><img class="center-img" src="../images/Group-106.png" class="img-fluid"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- navbar header-->
+
+        <body>
+
+
+            <!-- js External Bootstrap-->
+            <script src="../js/bootstrap.bundle.min.js"></script>
+            <script src="../js/bootstrap.min.js"></script>
+            <script src="../js/navbar.js"></script>
---
+++ dashboard/index.php
@@ -0,0 +1,116 @@
+<?php include("header.php") ?>
+
+
+        <div class="container">
+            <!-- chart -->
+            <div class="row">
+                <!-- chats sale -->
+                <div class="col-6">
+                    <canvas id="sale-today"></canvas>
+                </div>
+                <!-- chats sale -->
+
+                <!-- chart order -->
+                <div class="col-6">
+                    <canvas id="order-today"></canvas>
+                </div>
+                <!-- chart order -->
+            </div>
+            <!-- chart -->
+
+            <!-- All orders -->
+            <div class="row mt-5 align-content-center mb-5">
+                <p class="h4 border-83838 border-bottom border-1 pb-3">سفارشات امروز</p>
+                <!-- navbar -->
+                <div class="row col-12 justify-content-center border-83838 border-bottom border-1 pb-3">
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item border-0 ps-5">سفارش</li>
+                        <li class="list-group-item border-0 ps-5">وضعیت</li>
+                        <li class="list-group-item border-0 ps-5">افراد</li>
+                        <li class="list-group-item border-0 ps-5">محل</li>
+                        <li class="list-group-item border-0 ps-5">تاریخ سفارش</li>
+                        <li class="list-group-item border-0 ps-5">تاریخ مقصد</li>
+                    </ul>
+                </div>
+                <!-- navbar -->
+
+                <!-- order 1-->
+                <div class="row col-12 justify-content-center border-83838 border-bottom border-1 py-3">
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item border-0 ps-5">#۱۲۰۹۰۶</li>
+                        <li class="list-group-item border-0 ps-5 text-A717FF">سفارش جدید</li>
+                        <li class="list-group-item border-0 ps-5"><img class="img-fluid" src="../images/Ellipse 12.png" style="position:absolute;z-index:1; right: 20px !important;"><img class="img-fluid" src="../images/Ellipse 13.png" style="position:absolute;z-index:2; right: 40px !important;"><img class="img-fluid" src="../images/Ellipse 14.png" style="position:absolute;z-index:3; right: 60px !important;"></li>
+
+                        <li class="list-group-item border-0 ps-5">ایران _ تهران</li>
+                        <li class="list-group-item border-0 ps-5">۱۴۰۱/۴/۸</li>
+                        <li class="list-group-item border-0 ps-5">۱۴۰۱/۴/۲۸</li>
+                    </ul>
+                </div>
+                <!-- order 1-->
+
+                <!-- order 2-->
+                <div class="row col-12 justify-content-center border-83838 border-bottom border-1 py-3">
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item border-0 ps-5">#۱۲۰۹۰۶</li>
+                        <li class="list-group-item border-0 ps-5 text-11CE00">ارسال شده</li>
+                        <li class="list-group-item border-0 ps-5"><img class="img-fluid" src="../images/Ellipse 12.png" style="position:absolute;z-index:1; right: 20px !important;"><img class="img-fluid" src="../images/Ellipse 13.png" style="position:absolute;z-index:2; right: 40px !important;"><img class="img-fluid" src="../images/Ellipse 14.png" style="position:absolute;z-index:3; right: 60px !important;"></li>
+
+                        <li class="list-group-item border-0 ps-5">ایران _ تهران</li>
+                        <li class="list-group-item border-0 ps-5">۱۴۰۱/۴/۸</li>
+                        <li class="list-group-item border-0 ps-5">۱۴۰۱/۴/۲۸</li>
+                    </ul>
+                </div>
+                <!-- order 2-->
+
+                <!-- order 3-->
+                <div class="row col-12 justify-content-center border-83838 border-bottom border-1 py-3">
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item border-0 ps-5">#۱۲۰۹۰۶</li>
+                        <li class="list-group-item border-0 ps-5 text-FF1717">کنسل شده</li>
+                        <li class="list-group-item border-0 ps-5"><img class="img-fluid" src="../images/Ellipse 12.png" style="position:absolute;z-index:1; right: 20px !important;"><img class="img-fluid" src="../images/Ellipse 13.png" style="position:absolute;z-index:2; right: 40px !important;"><img class="img-fluid" src="../images/Ellipse 14.png" style="position:absolute;z-index:3; right: 60px !important;"></li>
+
+                        <li class="list-group-item border-0 ps-5">ایران _ تهران</li>
+                        <li class="list-group-item border-0 ps-5">۱۴۰۱/۴/۸</li>
+                        <li class="list-group-item border-0 ps-5">۱۴۰۱/۴/۲۸</li>
+                    </ul>
+                </div>
+                <!-- order 3-->
+
+                <!-- order 4-->
+                <div class="row col-12 justify-content-center border-83838 border-bottom border-1 py-3">
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item border-0 ps-5">#۱۲۰۹۰۶</li>
+                        <li class="list-group-item border-0 ps-5 text-ECB800">درحال بررسی</li>
+                        <li class="list-group-item border-0 ps-5"><img class="img-fluid" src="../images/Ellipse 12.png" style="position:absolute;z-index:1; right: 20px !important;"><img class="img-fluid" src="../images/Ellipse 13.png" style="position:absolute;z-index:2; right: 40px !important;"><img class="img-fluid" src="../images/Ellipse 14.png" style="position:absolute;z-index:3; right: 60px !important;"></li>
+
+                        <li class="list-group-item border-0 ps-5">ایران _ تهران</li>
+                        <li class="list-group-item border-0 ps-5">۱۴۰۱/۴/۸</li>
+                        <li class="list-group-item border-0 ps-5">۱۴۰۱/۴/۲۸</li>
+                    </ul>
+                </div>
+                <!-- order 4-->
+
+                <!-- order 5-->
+                <div class="row col-12 justify-content-center py-3">
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item border-0 ps-5">#۱۲۰۹۰۶</li>
+                        <li class="list-group-item border-0 ps-5 text-A717FF">سفارش جدید</li>
+                        <li class="list-group-item border-0 ps-5"><img class="img-fluid" src="../images/Ellipse 12.png" style="position:absolute;z-index:1; right: 20px !important;"><img class="img-fluid" src="../images/Ellipse 13.png" style="position:absolute;z-index:2; right: 40px !important;"><img class="img-fluid" src="../images/Ellipse 14.png" style="position:absolute;z-index:3; right: 60px !important;"></li>
+
+                        <li class="list-group-item border-0 ps-5">ایران _ تهران</li>
+                        <li class="list-group-item border-0 ps-5">۱۴۰۱/۴/۸</li>
+                        <li class="list-group-item border-0 ps-5">۱۴۰۱/۴/۲۸</li>
+                    </ul>
+                </div>
+                <!-- order 5-->
+            </div>
+            <!-- All orders -->
+            
+        </div>
+    </div>
+</div>
+</body>
+<script src="../js/Chart.min.js"></script>
+<script src="js/Charts.js"></script>
+
+</html>
